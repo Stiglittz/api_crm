b812b93637c474747faa87a0aad7d756276291c7
Hugo Gzz <hgogzzs@gmail.com>
2021-12-30 15:16:16 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["index.html", "./src/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// module.exports = {
+//   purge: ["index.html", "./src/**/*.jsx"],
+//   darkMode: false, // or 'media or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
